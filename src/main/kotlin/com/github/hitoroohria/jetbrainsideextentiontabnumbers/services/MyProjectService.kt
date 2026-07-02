feb4a0ad94b97855d9745b7068594d0ca042f191
GitHub Action <action@github.com>
2026-07-02 06:51:11 +0000

    Template cleanup

--- src/main/kotlin/com/github/hitoroohria/jetbrainsideextentiontabnumbers/services/MyProjectService.kt
+++ src/main/kotlin/com/github/hitoroohria/jetbrainsideextentiontabnumbers/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.hitoroohria.jetbrainsideextentiontabnumbers.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.hitoroohria.jetbrainsideextentiontabnumbers.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
